d972f01c4b74bf3f4986f36ed54ab225b60444df
Hunain Aslam <hunainaslam.ha@gmail.com>
2025-06-27 01:51:06 +0500

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import HomePage from "@/components/HomePage";
 import Banner from "@/components/Banner";
 
 type Props = {};
-
+export const metadata = {
+  title: "Pokémon Team Builder",
+  description: "Build your dream Pokémon team!",
+};
 const page = (props: Props) => {
   return (
     <main className="my-3">
